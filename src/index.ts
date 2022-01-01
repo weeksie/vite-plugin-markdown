38964d1b5eb948ba626187e57f7c626203ee6e57
Scotty Weeks <scott.weeks@gmail.com>
2022-01-01 10:20:33 -0500

    feat(esbuild) tsx -> jsx

--- src/index.ts
+++ src/index.ts
@@ -117,7 +117,7 @@ const tf = (code: string, id: string, options: PluginOptions): TransformResult =
         Object.keys(props).forEach(function (key) {
           SubReactComponent[key] = props[key]
         })
-        ${require('esbuild').transformSync(reactCode, { loader: 'tsx' }).code}
+        ${require('esbuild').transformSync(reactCode, { loader: 'jsx' }).code}
         return markdown
       }
     `
